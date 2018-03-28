4c27643257b8eebeb23570cbe39652fbff961e21
guerrerov270 <jguerrero089@gmail.com>
2018-03-28 14:06:16 -0500

    Creación del proyecto

---
+++ src/main/java/co/uq/pmvpedidos/app/SpringBootPmvPedidosApplication.java
@@ -0,0 +1,12 @@
+package co.uq.pmvpedidos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPmvPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPmvPedidosApplication.class, args);
+	}
+}
---
+++ src/test/java/co/uq/pmvpedidos/app/SpringBootPmvPedidosApplicationTests.java
@@ -0,0 +1,16 @@
+package co.uq.pmvpedidos.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootPmvPedidosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
