60e2ee3f7bf172c0f8f0c404e94622d6a6d031f9
guerrerov270 <jguerrero089@gmail.com>
2018-05-02 13:36:51 -0500

    Config para construir war

--- src/main/java/co/uq/pmvpedidos/app/SpringBootPmvPedidosApplication.java
+++ src/main/java/co/uq/pmvpedidos/app/SpringBootPmvPedidosApplication.java
@@ -3,17 +3,24 @@ package co.uq.pmvpedidos.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import co.uq.pmvpedidos.app.models.service.IUploadFileService;
 
 @SpringBootApplication
-public class SpringBootPmvPedidosApplication {
+public class SpringBootPmvPedidosApplication extends SpringBootServletInitializer {
 
 	@Autowired
 	IUploadFileService uploadFileService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPmvPedidosApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootPmvPedidosApplication.class);
 	}
 
 }
