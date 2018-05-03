42b5bda95aa7af0b4253d83586f9296cd9a24300
guerrerov270 <jguerrero089@gmail.com>
2018-05-02 20:53:40 -0500

    REconfigurando para despliegue en EC2

--- src/main/java/co/uq/pmvpedidos/app/SpringBootPmvPedidosApplication.java
+++ src/main/java/co/uq/pmvpedidos/app/SpringBootPmvPedidosApplication.java
@@ -4,23 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import co.uq.pmvpedidos.app.models.service.IUploadFileService;
 
 @SpringBootApplication
-public class SpringBootPmvPedidosApplication extends SpringBootServletInitializer {
+public class SpringBootPmvPedidosApplication {
 
 	@Autowired
 	IUploadFileService uploadFileService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPmvPedidosApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootPmvPedidosApplication.class);
 	}
 
 }
