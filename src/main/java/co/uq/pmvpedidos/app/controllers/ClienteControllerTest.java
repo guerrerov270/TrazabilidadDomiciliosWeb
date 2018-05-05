a9976defd8be007a713440cb201a3bb1154e9fb9
guerrerov270 <jguerrero089@gmail.com>
2018-05-05 00:24:09 -0500

    Implementación de pruebas de de cobertura con jacoco and sonar

---
+++ src/main/java/co/uq/pmvpedidos/app/controllers/ClienteControllerTest.java
@@ -0,0 +1,104 @@
+package co.uq.pmvpedidos.app.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClienteControllerTest {
+
+	@Test
+	public final void testVer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testListar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCrear() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testEditar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGuardar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testEliminar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetZonas() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
